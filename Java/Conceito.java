f0795b41b438ec6fde78c573517ff21ab6fa9771
Diogo Gomes <diogo.gomes@liferay.com>
2022-03-06 17:09:56 -0300

    novas classes java

---
+++ Java/Conceito.java
@@ -0,0 +1,36 @@
+public class Conceito {
+}
+/**
+ * variáveis:
+ *
+ *um espaço alocado na memoria do computador para armazenar algum tipo de valor
+ *
+ * tipos de variáveis?
+ * Instância> que pertence a um objeto
+ * Classe que pertence a uma classe
+ * Local
+ * parâmetro> relacionado a assinatura do método
+ *
+ *
+ * Padrões de definições:
+ *
+ * Visibilidade:
+ * public;
+ * protected
+ * private
+ *
+ * Modificador:
+ * static
+ * final = variável constante que não irá mudar
+ *
+ * Tipo:
+ * tipo de dado
+ *
+ * Nome
+ *nome da variável
+ *
+ * Valor Inicial
+ *
+ * pode haver um ou null =0
+ * os valores são:
+ */
---
+++ Java/Metodo.java
@@ -0,0 +1,41 @@
+public class Metodo {
+}
+
+/**
+ * método:
+ * é um pedaço de código (sub-rotina)
+ * disponibilizada por uma classe. que pode ser executada quando
+ * é feita uma requisição dela. elas são  responsáveis por definir
+ * e realizar um determinado comportamento
+ *
+ *
+ * Padrões de definições:
+ *
+ *   Visibilidade:
+ *   public;
+ *   protected;
+ *   private;
+ *
+ *   Tipo :
+ *   concreto
+ *   Abstrato
+ *
+ *   Retorno
+ *   tipo de dado ou "void" <- vazio
+ *
+ *  Nome fornecido ao método
+ *
+ *   Parametro
+ *
+ *   exceções
+ *   para prever erros possíveis
+ *
+ *
+ *   Modificador:
+ *   static
+ *   final = variável constante que não irá mudar
+ *
+ *
+ *
+ *   Nome
+ */
