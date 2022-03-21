bf7f1119f29e736d06a4c58f5459829daefc19e7
Diogo Gomes <diogo.gomes@liferay.com>
2022-03-21 18:23:59 -0300

    java script básico variaveis

---
+++ javaScript básico/variaveis em js/script.js
@@ -0,0 +1,12 @@
+//bolean
+var verdadeiroOuFalso = false;
+console.log(typeof(verdadeiroOuFalso));
+
+//numero
+var numero = 3;
+console.log(typeof(numero));
+
+//string
+
+var letras = 'diogo';
+console.log(typeof(letras));
